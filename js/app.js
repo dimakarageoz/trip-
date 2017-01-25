8b97e02f63c369855396c2d10d642a71601bb157
dimakarageoz <dkarageoz@gmail.com>
2017-01-25 18:13:06 +0200

    3

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
-var	my_news	=	[
-		{
-				author:	'Саша	Печкин',
-				text:	'В	четверг,	четвертого	числа...'
-		},
-		{
-				author:	'Просто	Вася',
-				text:	'Считаю,	что	$	должен	стоить	35	рублей!'
-		},
-		{
-				author:	'Гость',
-				text:	'Бесплатно.	Скачать.	Лучший	сайт	-	http://localhost:2000'
-		}
-];
+
+var	my_name	=	['Your friends','Message','Groups','Trip','Foto','Option'];
 
 // this.props.data.map((item, index)=>{
 //   return(
@@ -30,48 +18,152 @@ var	my_news	=	[
 // 			</div>
 // 		);
 // 	}
+// // });
+// var Author =React.createClass({
+// 	render:()=>{
+// 		var data= this.props.data;
+//
+// 		var goodAuthor =data.map((item, index)=>{
+// 			return(
+// 				<div key={index}>
+// 						<a href="#">{item}</a>
+// 				</div>
+// 			)
+// 		})
+// 		return(
+// 			<div	className="nickname">
+// 					{goodAuthor}
+// 			</div>
+// 		);
+// 	}
 // });
-var	News	=	React.createClass({
+var	Button	=	React.createClass({
+		handleOnClick: (e) => {
+		location.href = "https://www.google.com.ua/";
+		},
 		render:	function()	{
 				var	data	=	this.props.data;
+				var handleOnClick = this.handleOnClick;
+
 				var	newsTemplate	=	data.map(function(item,	index)	{
-						return	(
+					if(item ==='Foto')
+					{
+						return(
+						<div	key={index}>
+								<a	href="@" className="news__button">{item}</a>
+						</div>)
+					}
+					else if(item ==='Message')
+					{
+						return(
+						<div	key={index}>
+								<a	href="$" className="news__button">{item}</a>
+						</div>)
+					}
+					else {
+						return(
 								<div	key={index}>
-										<p	className="news__author">{item.author}:</p>
-										<p	className="news__text">{item.text}</p>
-								</div>
-						)
+										<button	className="news__button" onClick={handleOnClick}>{item}</button>
+									</div>)
+					}
 				})
-        console.log(newsTemplate);
 				return	(
-						<div	className="news">
+						<div	className="btg">
 								{newsTemplate}
 						</div>
 				);
 		}
 });
 
-var Comments=React.createClass({
+var my_nick = ['Pulli'];
+var	Author	=	React.createClass({
+		render:	function()	{
+				var	data	=	this.props.data;
+				var	newsTemplate	=	data.map(function(item,	index)	{
+					if(item==='Exit')
+				{return	(
+						<div	key={index}>
+								<a	href="#!#" className="news__button" >{item}:</a>
+						</div>
+				)} else
+						{return	(
+								<div	key={index}>
+										<a	href="#" className="news__button" >{item}:</a>
+								</div>
+						)}
+				})
+				return	(
+						<div	className=".list-group-item.dropdown-toggle">
+								{newsTemplate}
+						</div>
+				);
+		}
+});
+////////////////
+// var MySelect = React.createClass({
+//      getInitialState: function() {
+//          return {
+//              value: 'select'
+//          }
+//      },
+//      change: function(event){
+//          this.setState({value: event.target.value});
+//      },
+//      render: function(){
+//         return(
+//            <div>
+//                <select id="lang" onChange={this.change} value={this.state.value}>
+//                   <option value="select">Select</option>
+//                   <option value="Java">Java</option>
+//                   <option value="C++">C++</option>
+//                </select>
+//            </div>
+//         );
+//      }
+// });
+// // //////////////////////
+// var Select = React.createClass({
+//   getInitialState: function () {
+//     return {
+//       value: '?',
+//       options: [
+//         { value: null, name: 'Select…' },
+//         { value: 'a', name: 'A' },
+//         { value: 'b', name: 'B' },
+//         { value: 'c', name: 'C' }
+//       ]
+//     };
+//   },
+//   handleChange: function (e) {
+//     this.setState({value: e.target.value});
+//   },
+//   render: function () {
+//     var createItem = function (item, key) {
+//       return <option key={key} value={item.value}>{item.name}</option>;
+//     };
+//     return (
+//       <div>
+//         <select onChange={this.handleChange} value={this.state.value} onClick='location.href="https://www.google.com.ua/"'>
+//           {this.state.options.map(createItem)}
+//         </select>
+//         <a herf="#">Favorite letter: {this.state.value}</a>
+//       </div>
+//     );
+//   }
+// });
+// ///////////////////////
+var Bar=React.createClass({
 	render: ()=>{
 		return(
-		<div className="comments">
-		No new - no comments
-		</div>
-	);
-	}
+			<div className="navmenu navmenu-default navmenu-fixed-left offcanvas in canvas-slid">
+          <Author data={my_nick} />
+					<Button data={my_name} />
+					<Author data={['Exit']} />
+			</div>
+		); }
 });
-				var App=React.createClass({
-					render: ()=>{
-						return(
-							<div className="app">
-						    	Hello. I can show news!
-									<News data={my_news} />
-									<Comments />
-							</div>
-						); }
-				});
 
-				ReactDOM.render(
-				 <App />,
-				 document.getElementById('root')
-				);
+ReactDOM.render(
+ <Bar />,
+ document.getElementById('root')
+);
